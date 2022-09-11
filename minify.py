b2691e8a232df152f896f96e758cb97b48c17489
Bye <bye@byecorps.com>
2022-09-11 20:24:55 +0100

    ok it works now

--- minify.py
+++ minify.py
@@ -5,20 +5,24 @@ load_dotenv()
 
 tinify.key = os.environ.get("tinify_key")
 
-output_dir = "build/"
+output_dir = "build"
 
-blacklist = [
-    "logothing.png"
-]
+try:
+    os.mkdir(output_dir)
+except FileExistsError:
+    print("It's already there!!!")
+
+output_dir += "/"
 
 for ff in os.listdir():
+    print(ff)
     if ff.endswith(".png"):
         if (ff == "logothing.png"):
             continue
-        print(ff)
+
         source = tinify.from_file(ff)
         source.to_file(f'{output_dir}{ff}')
-    elif ff.endswith(".mp3"):
+    if ff.endswith(".mp3"):
         fle = open(ff, "rb")
         dst = open(f'{output_dir}{ff}', "wb+")
         dst.write(fle.read())
@@ -26,7 +30,10 @@ for ff in os.listdir():
         dst.close()
 
 os.system(f"terser lzs.js letters.js game.js --mangle --compress -o {output_dir}code.js")
-os.system(f"rm {output_dir}dbh.zip")
+try:
+    os.system(f"rm {output_dir}dbh.zip")
+except:
+    print("no need to delete anything")
 os.system(f"advzip {output_dir}dbh.zip --add build --shrink-insane -4")
 
 progress = os.path.getsize(f"{output_dir}dbh.zip") / 1024
